1c481a5d484b0454a655645a945fc3765c06ef9e
Nand Lal <nandlal@Nands-MacBook-Pro.local>
2025-08-30 18:33:18 +0500

    import useMemo in the DndProvider.js

--- src/components/DndProvider.js
+++ src/components/DndProvider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { DndProvider } from 'react-dnd';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { HTML5Backend } from 'react-dnd-html5-backend';
